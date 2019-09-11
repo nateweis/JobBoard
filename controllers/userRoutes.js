8a986f39f87d4c3ed18783c21c2296ae157dfef5
Naftali Weiss <Naftali@PROPUMP.local>
2019-09-11 08:57:16 -0600

    made a route to get a user from the database and tested

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js')
 
+router.get('/', User.login);
 
 module.exports = router
--- models/user.js
+++ models/user.js
@@ -1,6 +1,17 @@
 const db = require('../db/db_connection');
 
 const login = (req, res) => {
+    console.log(req.body);
     db.one('SELECT * FROM users WHERE username = ${username} AND password = ${password}', req.body)
-    .then()
+    .then((data) => {
+        res.json({info:data, message:"success"})
+    })
+    .catch((err) => {
+      res.json({err:err})
+    })
 }
+
+module.exports = {
+    login
+}
+
