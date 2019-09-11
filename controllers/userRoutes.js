55ed54a61e0b32d2850340e65109393fb85874c7
Naftali Weiss <Naftali@PROPUMP.local>
2019-09-11 12:01:02 -0600

    added cors to app

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js')
 
-router.get('/', User.login);
+router.post('/', User.login);
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-// /////////////////////////////////////////
-// Configuarations 
-// ////////////////////////////////////////
 
 const express = require('express');
 const app = express();
+const  cors = require('cors')
 
 const port = process.env.PORT || 3000;
 
 // /////////////////////////////////////////
 // MiddleWear
 // ////////////////////////////////////////
+const corsOption = {
+    origin:'http://localhost:3001',
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false
+  }
+  app.use(cors(corsOption))
+
 app.use(express.json());
+
 
 // /////////////////////////////////////////
 // Routes
@@ -18,8 +25,6 @@ app.use(express.json());
 const userController = require('./controllers/userRoutes')
 app.use('/users', userController);
 
-// /////////////////////////////////////////
-// Listener
-// ////////////////////////////////////////
+
 
 app.listen(port, () => {console.log(`Port ${port} is listening for the job board app`)}  )
