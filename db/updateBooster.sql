72608a14908c0fc7dcf1384892c2cf3933c2d845
Naftali Weiss <Naftali@PROPUMP.local>
2019-09-18 12:26:14 -0400

    made a update route but need to add in conditons for if no info

---
+++ db/updateBooster.sql
@@ -0,0 +1,27 @@
+-- 'UPDATE booster_jobs SET job_order_number = ${job_order_number}, notes={notes}, shipdate_packlist={shipdate_packlist}, completed={completed}, due_date={due_date}, 
+-- controller_received={controller_received}, controller_eta={controller_eta}, controller_po={controller_po}, last_updated=(SELECT NOW()::DATE), updated_by={user}, 
+-- pump_received={pump_received}, pump_eta={pump_eta}, pump_po={pump_po}, stage={stage}, job_address={job_address}, requested_by ={requested_by},  description = {description}, 
+--  date_created =${data_created}  WHERE id = {id}'
+
+UPDATE booster_jobs
+SET
+job_order_number = ${job_order_number},
+notes={notes},
+shipdate_packlist={shipdate_packlist},
+completed={completed},
+due_date={due_date},
+controller_received={controller_received},
+controller_eta={controller_eta},
+controller_po={controller_po},
+last_updated=(SELECT NOW()::DATE),
+updated_by={user},
+pump_received={pump_received},
+pump_eta={pump_eta},
+pump_po={pump_po},
+stage={stage},
+job_address={job_address},
+requested_by ={requested_by},
+description = {description}
+WHERE id = {id};
+
+
--- models/booster.js
+++ models/booster.js
@@ -31,11 +31,17 @@ const showScreen = (req, res) => {
 
 //update the booster job
 const updateJob = (req, res) => {
-   db.none('UPDATE booster_jobs SET job_order_number = ${job_order_number}, notes={notes}, shipdate_packlist={shipdate_packlist}, completed={completed}, due_date={due_date}, controller_received={controller_received}, controller_eta={controller_eta}, controller_po={controller_po}, last_updated=(SELECT NOW()::DATE), updated_by={user}, pump_received={pump_received}, pump_eta={pump_eta}, pump_po={pump_po}, stage={stage}, job_address={job_address}, requested_by ={requested_by},  description = {description},  date_created =${data_created}  WHERE id = {id}',req.body)
-   
-   jwt.verify(req.token, 'feedmecmore', (err, userInfo) => {
-    if(err){res.json({message:'403 forbiddin'})} 
-    else{res.json(req.body)}
+    console.log(req.body);
+   db.none('UPDATE booster_jobs SET job_order_number = ${job_order_number}, notes=${notes}, shipdate_packlist=${shipdate_packlist}, completed=${completed}, due_date=${due_date}, controller_received=${controller_received}, controller_eta=${controller_eta}, controller_po=${controller_po}, last_updated=(SELECT NOW()::DATE), updated_by=${user}, pump_received=${pump_received}, pump_eta=${pump_eta}, pump_po=${pump_po}, stage=${stage}, job_address=${job_address}, requested_by =${requested_by},  description = ${description},  date_created =${date_created}  WHERE id = ${id}',req.body)
+   .then(() => {   
+    jwt.verify(req.token, 'feedmecmore', (err, userInfo) => {
+        if(err){res.json({message:'403 forbiddin'})} 
+        else{res.json({message:"check it out, it worked"})}
+       })
+   })
+   .catch((err) => {
+       console.log(err);
+       res.send(err)
    })
 }
 
