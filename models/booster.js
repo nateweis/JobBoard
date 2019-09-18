114ccf51703030d5c268ac296d3fc8e2a2ade6a1
Naftali Weiss <Naftali@PROPUMP.local>
2019-09-18 15:30:40 -0400

    made a external queryfile for space and concected it to the update route

--- models/booster.js
+++ models/booster.js
@@ -1,5 +1,7 @@
 const db = require('../db/db_connection');
 const jwt = require('jsonwebtoken');
+const path = require('path');
+const QueryFile = require('pg-promise').QueryFile;
 
 const indexScreen = (req, res) => {
     db.any('SELECT booster_jobs.id, booster_jobs.job_order_number, booster_jobs.description, booster_jobs.requested_by, booster_jobs.job_address, booster_jobs.stage FROM booster_jobs')
@@ -16,6 +18,8 @@ const indexScreen = (req, res) => {
     })
 }
 
+
+
 const showScreen = (req, res) => {
     db.one('SELECT * FROM booster_jobs WHERE id = $1', req.params.id)
     .then((data) => {
@@ -29,10 +33,16 @@ const showScreen = (req, res) => {
     })
 }
 
+function sql(file) {
+    const fullPath = path.join(__dirname, file); 
+    return new QueryFile(fullPath, {minify: true});
+}
+const sqlUpdateBooster = sql('../db/updateBooster.sql')
+
 //update the booster job
 const updateJob = (req, res) => {
     console.log(req.body);
-   db.none('UPDATE booster_jobs SET job_order_number = ${job_order_number}, notes=${notes}, shipdate_packlist=${shipdate_packlist}, completed=${completed}, due_date=${due_date}, controller_received=${controller_received}, controller_eta=${controller_eta}, controller_po=${controller_po}, last_updated=(SELECT NOW()::DATE), updated_by=${user}, pump_received=${pump_received}, pump_eta=${pump_eta}, pump_po=${pump_po}, stage=${stage}, job_address=${job_address}, requested_by =${requested_by},  description = ${description},  date_created =${date_created}  WHERE id = ${id}',req.body)
+   db.none(sqlUpdateBooster, req.body)
    .then(() => {   
     jwt.verify(req.token, 'feedmecmore', (err, userInfo) => {
         if(err){res.json({message:'403 forbiddin'})} 
