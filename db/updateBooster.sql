6acd39648d6e1489b488f417aa7fdce6e291b3f4
Naftali Weiss <Naftali@PROPUMP.local>
2019-09-19 09:32:29 -0400

    the update route has been made and now if the feild is empty it will still update (but field needs to be sent)

--- db/updateBooster.sql
+++ db/updateBooster.sql
@@ -7,23 +7,24 @@
 
 UPDATE booster_jobs
 SET
-job_order_number = ${job_order_number},
-notes=${notes},
-shipdate_packlist=${shipdate_packlist},
-completed=${completed},
-due_date=${due_date},
-controller_received=${controller_received},
-controller_eta=${controller_eta},
-controller_po=${controller_po},
+job_order_number =(SELECT NULLIF(${job_order_number} ,NULL)),
+notes=(SELECT NULLIF(${notes} ,NULL)),
+shipdate_packlist=(SELECT NULLIF(${shipdate_packlist} ,NULL)),
+completed=(SELECT NULLIF(${completed} ,NULL)::boolean),
+due_date=(SELECT TO_DATE((SELECT NULLIF(${due_date} ,NULL)),'YYYY-MM-DD')),
+controller_received=(SELECT NULLIF(${controller_received} ,NULL)::boolean),
+controller_eta=(SELECT TO_DATE((SELECT NULLIF(${controller_eta} ,NULL)),'YYYY-MM-DD')),
+controller_po=(SELECT NULLIF(${controller_po} ,NULL)),
 last_updated=(SELECT NOW()::DATE),
 updated_by=${user},
-pump_received=${pump_received},
-pump_eta=${pump_eta},
-pump_po=${pump_po},
-stage=${stage},
-job_address=${job_address},
-requested_by =${requested_by},
-description = ${description}
+pump_received=(SELECT NULLIF(${pump_received} ,NULL)::boolean),
+pump_eta=(SELECT TO_DATE((SELECT NULLIF(${pump_eta} ,NULL)), 'YYYY-MM-DD')),
+pump_po =(SELECT NULLIF(${pump_po},NULL)),
+stage=(SELECT NULLIF(${stage} ,NULL)),
+job_address=(SELECT NULLIF(${job_address} ,NULL)),
+requested_by =(SELECT NULLIF(${requested_by} ,NULL)),
+description = (SELECT NULLIF(${description} ,NULL))
 WHERE id = ${id};
+
 
 
