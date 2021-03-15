fc5a46a32c51408e5827f445d3b118399f956bfd
Naftali Weiss <Naftali@PROPUMP.local>
2021-03-15 11:43:46 -0400

    attempting to adjust database string

--- db/db_connection.js
+++ db/db_connection.js
@@ -2,9 +2,15 @@ const promise = require('bluebird');
 const options = {
     promiseLib : promise
 }
+let ssl = null;
 const pgp = require('pg-promise')( options);
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:uspumpdatabase@localhost:5432/job_board';
-const db = pgp(connectionString);
+const cString = process.env.DATABASE_URL || 'postgres://postgres:uspumpdatabase@localhost:5432/job_board';
+const config = {
+    connectionString: cString,
+    max:30,
+    ssl: ssl
+}
+const db = pgp(config);
 db.connect();
 
 module.exports = db
